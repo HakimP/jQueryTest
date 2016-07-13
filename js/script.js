dbe514a4c32aa1531c72eba41e5dabb1be77da2e
HakimP <pedrohakim0@gmail.com>
2016-07-12 21:27:36 -0500

    ejercicio

---
+++ js/script.js
@@ -0,0 +1,44 @@
+function clickTodasAnimaciones() {
+	$('#circulo1').jAnimateSequence(['bounce', 'tada', 'wobble',
+									 'flipOutX', 'fadeOutDown', 'fadeOutDownBig',
+									 'flipInX', 'rotateOutDownLeft', 'zoomOutRight',
+									 'slideOutUp', 'hinge','rollIn', 'rollOut',
+									 'zoomIn','zoomInDown','zoomInLeft','zoomInRight',
+									 'zoomInUp','zoomOut','zoomOutDown','zoomOutLeft',
+									 'zoomOutRight','zoomOutUp','slideInDown','slideInLeft',
+									 'slideInRight','slideInUp','slideOutDown','slideOutLeft',
+									 'slideOutRight','slideOutUp']);
+}
+function clickFlipOutX () {
+	$('#circulo1').jAnimateOnce('flipOutX');
+}
+
+function clickBounce () {
+	$('#circulo1').jAnimateOnce('bounce');
+}
+
+function clickTada () {
+	$('#circulo1').jAnimateOnce('tada');
+}
+
+function clickWobble() {
+	$('#circulo1').jAnimateOnce('wobble');
+}
+function clickFadeOutDown () {
+	$('#circulo1').jAnimateOnce('fadeOutDown');
+}
+function clickFadeOutDownBig () {
+	$('#circulo1').jAnimateOnce('fadeOutDownBig');
+}
+function clickFlipInX () {
+	$('#circulo1').jAnimateOnce('flipInX');
+}
+function clickrRtateOutDownLeft () {
+	$('#circulo1').jAnimateOnce('rotateOutDownLeft');
+}
+function clickZoomOutRight () {
+	$('#circulo1').jAnimateOnce('zoomOutRight');
+}
+function clickSlideOutUp () {
+	$('#circulo1').jAnimateOnce('slideOutUp');
+}
